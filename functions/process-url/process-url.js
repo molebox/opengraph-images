c7b42e4497ecf28c5d2010bd8f984c483c775186
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-05-06 09:06:33 +0200

    testing

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -6,10 +6,13 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_SECRET
 });
 
+// https://vigilant-jones-f0730c.netlify.app/.netlify/functions/process-url?title=test&tags=test,test,test&author=@studio_hungry
+
 exports.handler = async function(event, ctx) {
     const {queryStringParameters} = event;
 
     try {
+        // https://res.cloudinary.com/richardhaines/image/upload/v1588746634/og-images/img-1.png
         const imageUrl = cloudinary.url(
             `${process.env.RICH_IMAGE_VERSION}/og-images/img-1.png`,
             {
